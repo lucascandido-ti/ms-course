1e798e3108134bb6cd335d28c9aa48f73ae621bc
lucas-candido <sistema2@innovationbrindes.com.br>
2021-10-14 21:07:08 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/lucascandidocourse/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.lucascandidocourse.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.lucascandidocourse.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(Long workerId, int days) {
+		return new Payment("Bob",200.0, days);
+	}
+}
